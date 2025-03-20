e0b316537d96edbb424f3bb6496657438ff1ec55
Caleb Mackey <caleb.mackey@bolton-menk.com>
2025-03-20 17:54:16 -0500

    chore: host on github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/open-source-gis/',
   plugins: [
     vue(),
     vueDevTools(),
+    basicSsl()
   ],
   resolve: {
     alias: {
